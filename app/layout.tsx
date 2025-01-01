ade89502eee477df5f6e66ab47a9345deb77e272
Samson Monyluak <samsonemes@gmail.com>
2025-01-01 23:10:18 +0300

    init

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import type { Metadata } from "next";
+import "@/styles/globals.css";
+import { fontSans } from "@/lib/fonts";
+import { meta } from "@/lib/meta";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(meta.url),
+  title: {
+    default: meta.title,
+    template: `%s - ${meta.title}`,
+  },
+  description: meta.description,
+  applicationName: meta.title,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: meta.url,
+    title: meta.title,
+    description: meta.description,
+    siteName: meta.title,
+    images: [
+      {
+        url: meta.ogImage,
+        width: 1200,
+        height: 630,
+        alt: meta.title,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: meta.title,
+    description: meta.description,
+    images: [meta.ogImage],
+    creator: "@samsonmonyluak",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${meta.url}/site.webmanifest`,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${fontSans.variable}  antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ lib/meta.ts
@@ -0,0 +1,6 @@
+export const meta = {
+  title: "Patterns",
+  description: "...",
+  url: "https://patterns.monyluak.com",
+  ogImage: "https://patterns.monyluak.com/open-graph.png",
+};
